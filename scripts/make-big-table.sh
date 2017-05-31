a048bf54364fa25d8337c5f5ff27565869b88f3f
Tobias Kuhn <kuhntobias@gmail.com>
2017-05-31 16:52:20 +0200

    Updated big table

--- scripts/make-big-table.sh
+++ scripts/make-big-table.sh
@@ -4,24 +4,25 @@
 # $ scripts/make-big-table.sh
 #
 
-echo "SOURCE,RELATION,AIDA-SENTENCE,VIA" \
+echo "SOURCE,RELATION,AIDA-SENTENCE,VIA,COMPONENTS" \
   > tables/all.csv
 
 tail -n +2 tables/papers.csv \
+  | awk '{ print $0"," }' \
   >> tables/all.csv
 
 tail -n +2 tables/alzheimers.csv \
-  | awk '{ print $0"," }' \
+  | awk '{ print $0",," }' \
   >> tables/all.csv
 
 tail -n +2 tables/openaccess.csv \
-  | awk '{ print $0"," }' \
+  | awk '{ print $0",," }' \
   >> tables/all.csv
 
 tail -n +2 tables/npuserstudy.csv \
-  | awk '{ print $0"," }' \
+  | awk '{ print $0",," }' \
   >> tables/all.csv
 
 tail -n +2 tables/npbotstudy.csv \
-  | sed -r 's/^([^,]*,[^,]*,".*"),[^,]*,[^,]*$/\1,/' \
+  | sed -r 's/^([^,]*,[^,]*,".*"),([^, ]* [^, ]*)$/\1,,\2/' \
   >> tables/all.csv
--- scripts/make-npbotstudy-table.sh
+++ scripts/make-npbotstudy-table.sh
@@ -4,14 +4,14 @@
 # $ scripts/make-npbotstudy-table.sh
 #
 
-echo "SOURCE,RELATION,AIDA-SENTENCE,TAX,GENE" \
+echo "SOURCE,RELATION,AIDA-SENTENCE,COMPONENTS" \
   > tables/npbotstudy.csv
 
 cat raw/npbotstudy.txt \
   | egrep -v '^#' \
   | egrep -v '^TAX,' \
   | sed -r 's/^([^,]*,[^,]*,[^,]*),([^"].*)$/\1,"\2"/' \
-  | sed -r 's/^([^,]*),([^,]*),([^,]*),(".*")$/\3,claims,\4,\1,\2/' \
+  | sed -r 's_^([^,]*),([^,]*),([^,]*),(".*")$_\3,claims,\4,http://purl.uniprot.org/taxonomy/\1 http://www.ncbi.nlm.nih.gov/gene/\2_' \
   | awk '{ print "http://identifiers.org/pubmed/"$0 }' \
   >> tables/npbotstudy.csv
 
